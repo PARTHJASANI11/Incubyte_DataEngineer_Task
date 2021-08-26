8640c579fdd1cdc70d18df7078fe51f78b03c9ec
PARTHJASANI11 <63165094+PARTHJASANI11@users.noreply.github.com>
2021-08-26 22:38:35 +0530

    Table Creation in Database for Each Country

--- Incubyte_DataEngineer_ScriptFile.php
+++ Incubyte_DataEngineer_ScriptFile.php
@@ -2,6 +2,12 @@
 
 $ReadData = file('Incubyte_DataEngineer_HospitalData.dat'); // Read a file and convert each row of data into an array
 $Country = []; // To store country's name
+$Connection = mysqli_connect("localhost", "root", "", "incubyte_dataengineer_database");
+
+if(!$Connection)
+{
+	die("Connection Failed:" . mysqli_connect_error());
+}
 
 if($ReadData)
 {
@@ -19,8 +25,42 @@ if($ReadData)
 		}
 		else // If data
 		{
-			echo "<h3> $Data[2]" . " " . "$Data[3]". " " . "$Data[4]" . " " . "$Data[5]" . " " . "$Data[6]" . " " . "$Data[7]" . " " . "$Data[8]" . " " . "$Data[9]" . " " . "$Data[10]" . " " . "$Data[11]";
-			echo " ";
+			// echo "<h3> $Data[2]" . " " . "$Data[3]". " " . "$Data[4]" . " " . "$Data[5]" . " " . "$Data[6]" . " " . "$Data[7]" . " " . "$Data[8]" . " " . "$Data[9]" . " " . "$Data[10]" . " " . "$Data[11]";
+			// echo " ";
+			$Temp_Country = $Data[9];
+			$Query = "SELECT 1 FROM `$Temp_Country`";
+			$Result = mysqli_query($Connection, $Query);
+
+			if($Result == False)
+			{
+				$Query_Table = "CREATE TABLE $Temp_Country (Customer_Id VARCHAR(18) PRIMARY KEY,
+														Customer_Name VARCHAR(255) NOT NULL,
+														Customer_Open_Date DATE NOT NULL,
+														Last_Consulted_Date DATE DEFAULT NULL,
+														Vaccination_Type CHAR(5) DEFAULT NULL,
+														Doctor_Consulted CHAR(255) DEFAULT NULL,
+														State CHAR(5) DEFAULT NULL,
+														Country CHAR(5) DEFAULT NULL,
+														Post_Code INT(8) DEFAULT NULL,
+														Default_Customer CHAR(1) DEFAULT NULL)";
+				
+				$Query_Result = mysqli_query($Connection, $Query_Table);
+
+				if($Query_Result)
+				{
+					echo "Success!";
+				}
+				else
+				{
+					echo "Failure!";
+				}
+			}
+			else
+			{
+				echo "Already Created!";
+			}
+
+
 		}
 
 
