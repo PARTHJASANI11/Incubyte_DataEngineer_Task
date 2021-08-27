76c0b481b4e637c731fa9a7ece0427eb522523be
PARTHJASANI11 <63165094+PARTHJASANI11@users.noreply.github.com>
2021-08-27 14:49:27 +0530

    Data insertion into respective country table

--- Incubyte_DataEngineer_ScriptFile.php
+++ Incubyte_DataEngineer_ScriptFile.php
@@ -2,36 +2,49 @@
 
 $ReadData = file('Incubyte_DataEngineer_HospitalData.dat'); // Read a file and convert each row of data into an array
 $Country = []; // To store country's name
-$Connection = mysqli_connect("localhost", "root", "", "incubyte_dataengineer_database");
+$Id = []; // To store registered customer's id
 
+
+// Database connection 
+//mysqli_connect("servername", "username", "password","databse")
+$Connection = mysqli_connect("localhost", "root", "", "incubyte_dataengineer_database"); 
 if(!$Connection)
 {
 	die("Connection Failed:" . mysqli_connect_error());
 }
 
+
+// Data Reading
 if($ReadData)
 {
-	foreach($ReadData as $Temp) // Loop through all the row of data from an array
+	// Loop through all the row of data from an array
+	foreach($ReadData as $Temp) 
 	{
-		$Data = explode("|", $Temp); // Create an array of elements for each row
+		// Create an array of elements for each row
+		$Data = explode("|", $Temp); 
 
-		if($Data[1] == "H") // If header, then continue reading next row
+		// If header, then continue reading next row
+		if($Data[1] == "H") 
 		{
 			continue;
 		}
-		else if($Data[1] == "T") // If trailer, then stop reading 
+
+		// If trailer, then stop reading 
+		else if($Data[1] == "T") 
 		{
 			break;
 		}
-		else // If data
+
+		else // If details, then process it
 		{
-			// echo "<h3> $Data[2]" . " " . "$Data[3]". " " . "$Data[4]" . " " . "$Data[5]" . " " . "$Data[6]" . " " . "$Data[7]" . " " . "$Data[8]" . " " . "$Data[9]" . " " . "$Data[10]" . " " . "$Data[11]";
-			// echo " ";
 			$Temp_Country = $Data[9];
-			$Query = "SELECT 1 FROM `$Temp_Country`";
+
+			// To check the presence of table in database
+			$Query = "SELECT 1 FROM `$Temp_Country`"; 
 			$Result = mysqli_query($Connection, $Query);
 
-			if($Result == False)
+			// To create a new table, if not exist 
+			if($Result == False) 
 			{
 				$Query_Table = "CREATE TABLE $Temp_Country (Customer_Id VARCHAR(18) PRIMARY KEY,
 														Customer_Name VARCHAR(255) NOT NULL,
@@ -41,35 +54,85 @@ if($ReadData)
 														Doctor_Consulted CHAR(255) DEFAULT NULL,
 														State CHAR(5) DEFAULT NULL,
 														Country CHAR(5) DEFAULT NULL,
-														Post_Code INT(8) DEFAULT NULL,
+														Post_Code INT(6) DEFAULT NULL,
+														Date_of_Birth Date DEFAULT NULL,
 														Default_Customer CHAR(1) DEFAULT NULL)";
 				
-				$Query_Result = mysqli_query($Connection, $Query_Table);
+				$Result_Table = mysqli_query($Connection, $Query_Table);
 
-				if($Query_Result)
+				// To insert the first record in newly created table
+				if($Result_Table) 
 				{
-					echo "Success!";
+					$Query_Insert = "INSERT INTO $Temp_Country(Customer_Id, Customer_Name, 
+																Customer_Open_Date, Last_Consulted_Date, 
+																Vaccination_Type, Doctor_Consulted, State, 
+																Country, Post_Code, Date_of_Birth, 
+																Default_Customer) 
+														VALUES('$Data[3]', '$Data[2]', $Data[4], $Data[5], 
+																'$Data[6]','$Data[7]', '$Data[8]', '$Data[9]', 
+																$Data[10], $Data[11], '$Data[12]')";
+					
+					$Result_Insert = mysqli_query($Connection, $Query_Insert);
+
+					// Success of table creation and first data entry
+					if($Result_Insert) 
+					{
+						echo "<h1> Table for". " <b> $Temp_Country </b> " ." is created successfully and first data is also inserted successfully! </h1>";
+						echo "<br>";
+						array_push($Id, $Data[3]);
+					}
+
+					// Failure of firt data entry
+					else 
+					{
+						echo " <h1> Data of Customer Id" . "<b> $Data[3] </b>" . "is not inserted successfully!" . mysqli_error($Connection) . "</h1>";
+						echo "<br>";
+					}
 				}
-				else
+
+				// Failure of table creation
+				else 
 				{
-					echo "Failure!";
+					echo "<h1> Table for" . "<b> $Temp_Country </b>" . "is not created successfully!" . mysqli_error($Connection) . "</h1>";
+					echo "<br>";
 				}
 			}
-			else
+
+			// If table is already created 
+			else 
 			{
-				echo "Already Created!";
+				// To add the new record in database
+				if(!in_array($Temp_Country, $Id)) 
+				{
+					$Query_Insert = "INSERT INTO $Temp_Country(Customer_Id, Customer_Name, Customer_Open_Date, Last_Consulted_Date, Vaccination_Type, Doctor_Consulted, State, Country, Post_Code, Date_of_Birth, Default_Customer) VALUES('$Data[3]', '$Data[2]', $Data[4], $Data[5], '$Data[6]', '$Data[7]', '$Data[8]', '$Data[9]', $Data[10], $Data[11], '$Data[12]')";
+					
+					$Result_Insert = mysqli_query($Connection, $Query_Insert);	
+
+					// Success of data insertion
+					if($Result_Insert)  
+					{
+						echo "<h1> Data of Customer Id" . "<b> $Data[3] </b>" . "is inserted successfully! </h1>";
+						echo "<br>";
+						array_push($Id, $Data[3]);
+					}
+
+					// Data already exist in database
+					else 
+					{
+						echo "<h1> Data of Customer Id". "<b> $Data[3] </b>" . "is already exists in database! <h1>";
+					    echo "<br>";
+					}
+				}
 			}
-
-
 		}
-
-
 	}
-
 }
 
+// If a file is empty
 else
 {
 	echo "<center> <h1> The File is Empty! </h1> </center>";
+	echo "<br>";
 }
+
 ?>
